731929a3fc2757486cb8ed0e801f33d85417fbee
Jodecir Neto <jodecirneto@gmail.com>
2021-09-06 12:08:01 -0300

    CREATE system skeleton

--- Aluno/Program.cs
+++ Aluno/Program.cs
@@ -6,7 +6,35 @@ namespace Aluno
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Criar Aluno");
+            Console.WriteLine("2 - Listar Aluno");
+            Console.WriteLine("3 - Calcular Média Geral dos Alunos");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X") {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                Console.WriteLine("Informe a opção desejada:");
+                Console.WriteLine("1 - Criar Aluno");
+                Console.WriteLine("2 - Listar Aluno");
+                Console.WriteLine("3 - Calcular Média Geral dos Alunos");
+                Console.WriteLine("X - Sair");
+                Console.WriteLine();
+            }
         }
     }
 }
