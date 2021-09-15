04edf001bb38d6003648e179b9209a273fcfe01d
Jodecir Neto <jodecirneto@gmail.com>
2021-09-15 12:36:06 -0300

    CREATE bank system

---
+++ Transferência bancária/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Transferência_bancária
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
