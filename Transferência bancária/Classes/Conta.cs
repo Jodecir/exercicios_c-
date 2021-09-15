6c667f4c980e05cf8d841f1ce0969e2bbdbf0b7a
Jodecir Neto <jodecirneto@gmail.com>
2021-09-15 17:32:46 -0300

    UPDATE account class in bank system

--- Transferência bancária/Classes/Conta.cs
+++ Transferência bancária/Classes/Conta.cs
@@ -3,8 +3,15 @@ namespace Banco
     public class Conta
     {  
         private TipoConta TipoConta { get; set; } 
-        public double Saldo { get; set; }  
-        public double Credito { get; set; }
-        public string Nome { get; set; }
+        private double Saldo { get; set; }  
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome) {
+          this.TipoConta = tipoConta;
+          this.Saldo = saldo;
+          this.Credito = credito;
+          this.Nome = nome;
+        }
     }
 }
