679f9f4ea80a222e45fad70297da9767cd35f334
Jodecir Neto <jodecirneto@gmail.com>
2021-09-21 14:44:11 -0300

    CREATE project with new console

---
+++ Cadastro de Séries/CadastroDeSéries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CadastroDeSéries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
