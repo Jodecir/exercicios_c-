3e262db94d43b5c65045a70bf7b656125fc693f0
Jodecir Neto <jodecirneto@gmail.com>
2021-09-22 19:18:06 -0300

    CREATE serie updater in Program.cs

--- Cadastro de Séries/Program.cs
+++ Cadastro de Séries/Program.cs
@@ -23,6 +23,7 @@ namespace Cadastro
                         VisualizarSerie();
                         break;
                     case "4":
+						AtualizarSerie();
                         break;
                     case "5":
                         break;
@@ -94,6 +95,36 @@ namespace Cadastro
                 var serie = repositorio.RetornaPorId(indiceSerie);
 
                 Console.WriteLine(serie);
+            }
+
+            static void AtualizarSerie()
+            {
+                Console.Write("Digite o id da série: ");
+                int indiceSerie = int.Parse(Console.ReadLine());
+
+                foreach (int i in Enum.GetValues(typeof(Genero)))
+                {
+                    Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+                }
+                Console.Write("Digite o gênero entre as opções acima: ");
+                int entradaGenero = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o Título da Série: ");
+                string entradaTitulo = Console.ReadLine();
+
+                Console.Write("Digite o Ano de Início da Série: ");
+                int entradaAno = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite a Descrição da Série: ");
+                string entradaDescricao = Console.ReadLine();
+
+                Serie atualizaSerie = new(id: indiceSerie,
+                                            genero: (Genero)entradaGenero,
+                                            titulo: entradaTitulo,
+                                            descricao: entradaDescricao,
+                                            ano: entradaAno);
+
+                repositorio.Atualiza(indiceSerie, atualizaSerie);
             }
 
             static string ObterOpcaoUsuario()
