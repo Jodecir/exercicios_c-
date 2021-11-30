df0e23474194f0b09fe318809445e22a46bc7067
Jodecir Neto <jodecirneto@gmail.com>
2021-11-30 12:32:13 -0300

    CREATE multidimensional array in collection

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,16 +1,34 @@
-﻿using System;
+﻿    using System;
 
-namespace Colecoes
-{
-    class Program
+    namespace Colecoes
     {
-        static void Main(string[] args)
+        internal static class Program
         {
-            int[] arrayInteiros = new int [3];
+        private static void Main(string[] args)
+        {
+            if (args == null)
+                throw new ArgumentNullException(nameof(args));
+
+                int[] arrayInteiros = new int[3];
+                int[,] matriz = new int[4, 2] {
+                    {8, 8},
+                    {2, 4},
+                    {8, 9},
+                    {1, 8},
+                };
 
             arrayInteiros[0] = 10;
             arrayInteiros[1] = 20;
             arrayInteiros[2] = 30;
+
+            System.Console.WriteLine("Percorrendo Matriz com For:");
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    System.Console.WriteLine(matriz[i, j]);
+                }
+            }
 
             System.Console.WriteLine("Percorrendo Array com For:");
             for (int i = 0; i < arrayInteiros.Length; i++)
